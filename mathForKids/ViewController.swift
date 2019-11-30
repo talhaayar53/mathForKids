e0d93a115efb7a957ceb3ea7574463d15cd9ff59
Talha <talhaayar53@gmail.com>
2019-11-30 08:58:30 +0300

    Initial Commit

---
+++ mathForKids/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  mathForKids
+//
+//  Created by TALHA AYAR on 30.11.2019.
+//  Copyright © 2019 Talha Ayar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
